d611180d5921218313920c8202e30cbcca7a0065
Jorgvt <vila_tronik@hotmail.com>
2024-10-28 16:28:35 +0100

    original original model

--- Training/train_original.py
+++ Training/train_original.py
@@ -31,7 +31,7 @@ from ml_collections import ConfigDict, config_flags
 import wandb
 from iqadatasets.datasets import *
 from JaxPlayground.utils.wandb import *
-from paramperceptnet.models import Baseline as PerceptNet
+from paramperceptnet.models import Original as PerceptNet
 from paramperceptnet.constraints import *
 from paramperceptnet.training import *
 from paramperceptnet.configs import original_config as config
@@ -92,36 +92,10 @@ state = state.replace(params=clip_param(state.params, "A", a_min=0))
 # %%
 state.params.keys()
 
-def check_trainable(path):
-    if "GDNGamma_0" in path:
-        if not config.TRAIN_GDNGAMMA:
-            return True
-    if "Color" in path:
-        if not config.TRAIN_JH:
-            return True
-    if "GDN_0" in path:
-        if not config.TRAIN_GDNCOLOR:
-            return True
-    if "CenterSurroundLogSigmaK_0" in path:
-        if not config.TRAIN_CS:
-            return True
-    if "GDNGaussian_0" in path:
-        if not config.TRAIN_GDNGAUSSIAN:
-            return True
-    if "Gabor" in "".join(path):
-        if not config.TRAIN_GABOR:
-            return True
-    if not config.A_GDNSPATIOFREQORIENT:
-        if ("GDNSpatioChromaFreqOrient_0" in path) and ("A" in path):
-            return True
-    if "GDNSpatioChromaFreqOrient_0" not in path and config.TRAIN_ONLY_LAST_GDN:
-        return True
-    return False
-
 # %%
 trainable_tree = freeze(
     flax.traverse_util.path_aware_map(
-        lambda path, v: "non_trainable" if check_trainable(path) else "trainable",
+        lambda path, v: "trainable",
         state.params,
     )
 )
