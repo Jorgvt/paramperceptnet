8ee5a8b8fbafdb846ffcf38692edfbe128cafde5
Jorgvt <vila_tronik@hotmail.com>
2025-03-26 11:36:14 +0100

    included recalculation of filters just in case

--- Plotting/no_train_point.py
+++ Plotting/no_train_point.py
@@ -1,6 +1,8 @@
 import os
+from tqdm.auto import tqdm
 
 import jax
+from jax import numpy as jnp
 import orbax.checkpoint
 import wandb
 from ml_collections import ConfigDict
@@ -49,17 +51,27 @@ for run in runs:
     for file in run.files():
         file.download(root="./models/", replace=True)
     ## 4. Generate the filters
+    ## 4.1. Load checkpoint 0
     orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
     data = orbax_checkpointer.restore(os.path.join("./models/","model-0"))
-    params = data["params"]
-    state = data["state"]
+    params = jax.tree_util.tree_map(lambda x: jnp.array(x), data["params"])
+    state = jax.tree_util.tree_map(lambda x: jnp.array(x), data["state"])
+    ## 4.2. Re-generate filters
+    _, state  = model.apply({"params": params, **state}, jnp.ones((1,384,512,3)), train=True, mutable=list(state.keys()))
     ## 5. Evaluate
-    distances, moses = [], []
-    for batch in dst_train.iter(batch_size=config.BATCH_SIZE):
-        batch = (batch["reference"], batch["distorted"], batch["mos"])
-        distance = compute_distance(params, state, batch[0], batch[1])
-        distances.extend(distance)
-        moses.extend(batch[2])
+    ## 5.1. Iterate over the two datasets
+    results = {}
+    for dst, name in tqdm(zip([dst_train, dst_val], ["TID08", "TID13"])):
+        distances, moses = [], []
+        for batch in tqdm(dst.iter(batch_size=config.BATCH_SIZE)):
+            batch = (batch["reference"], batch["distorted"], batch["mos"])
+            distance = compute_distance(params, state, batch[0], batch[1])
+            distances.extend(distance)
+            moses.extend(batch[2])
+            # break
+        corr = stats.pearsonr(distances, moses)[0]
+        results[(run.id, name)] = corr
+        print(f"id: {run.id} | Corr: {corr}")
         # break
-    print(f"id: {run.id} | Corr: {stats.pearsonr(distances, moses)[0]}")
-    # break
+
+print(results)
