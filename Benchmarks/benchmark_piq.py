a03ce7e271dba54f35ec577431fa85f3cf9b5a8d
Jorgvt <vila_tronik@hotmail.com>
2026-02-04 14:18:24 +0100

    missed fixing path after argparse. it is fine now

--- Benchmarks/benchmark_piq.py
+++ Benchmarks/benchmark_piq.py
@@ -22,7 +22,7 @@ data_path = args.data_path
 BATCH_SIZE = args.batch_size
 name = args.name if args.name is not None else os.path.basename(data_path.rstrip(os.sep))
 
-dataset = TID2008(path=os.path.join(data_path, "TID", "TID2008"))
+dataset = TID2008(path=data_path)
 dst_rdy = dataset.dataset.batch(BATCH_SIZE).prefetch(1)
 
 
